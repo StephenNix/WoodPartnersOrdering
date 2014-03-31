2964c04fe51d18623e82453e8469e8a2d1296994
Stephen Nix <stephencnix221@gmail.com>
2014-03-31 00:28:54 -0400

    #prettyButtons

--- js/effects_landing.js
+++ js/effects_landing.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         speedOut:   200, 
         hideOnContentClick  :   false,
         width   :   700,
-        height  :    720,
+        height  :    615,
         fitToView   :   false,
         autoSize    :   false
     });
@@ -31,16 +31,34 @@ $(document).ready(function() {
     
     $('#orderStatus').click(function() {
         $('#orderNumber_status input[type="number"]').val('');
+        if($('#orderNumber_revise').css('display') !== 'none') {
+            $('#orderNumber_revise').stop(true, true).slideToggle(200);
+        }
+        if($('#orderNumber_cancel').css('display') !== 'none') {
+            $('#orderNumber_cancel').stop(true, true).slideToggle(200);
+        }
         $('#orderNumber_status').stop(true, true).slideToggle(200);
     });
 
     $('#reviseOrder').click(function() {
         $('#orderNumber_revise input[type="number"]').val('');
+        if($('#orderNumber_status').css('display') !== 'none') {
+            $('#orderNumber_status').stop(true, true).slideToggle(200);
+        }
+        if($('#orderNumber_cancel').css('display') !== 'none') {
+            $('#orderNumber_cancel').stop(true, true).slideToggle(200);
+        }
         $('#orderNumber_revise').stop(true, true).slideToggle(200);
     });
 
     $('#cancelOrder').click(function() {
         $('#orderNumber_cancel input[type="number"]').val('');
+        if($('#orderNumber_revise').css('display') !== 'none') {
+            $('#orderNumber_revise').stop(true, true).slideToggle(200);
+        }
+        if($('#orderNumber_status').css('display') !== 'none') {
+            $('#orderNumber_status').stop(true, true).slideToggle(200);
+        }
         $('#orderNumber_cancel').stop(true, true).slideToggle(200);
     });
 });
